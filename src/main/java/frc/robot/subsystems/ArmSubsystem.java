ad4941e06bd1947bbad99821df4b0b3b98e594b6
Stephen Oz <31784207+SteptimusHeap@users.noreply.github.com>
2023-02-01 20:56:10 -0500

    Initial Commit

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,83 +8,80 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
-  TalonFX leftArmMotor = new TalonFX(?);//change ID number
-	PigeonIMU pidgeonGyro = new PigeonIMU(0);
-	Joystick willController = new Joystick(0);
-  Joystick oliviaController = new Joystick(1);
+  public final TalonFX leftArmMotor = new TalonFX(?);
+  public final TalonFX rightArmMotor = new TalonFX(?);
+  public final CANCoder leftCANCoder = new CANCoder(?);
+  public final CANCoder rightCANCoder = new CANCoder(?);
 
-  BaseMotorController rightArmMotor = new TalonFX(?);//change ID number //following leftArmMotor
+  public final PigeonIMU pidgeonGyro = new PigeonIMU(0);
+  public final double maxVelocity = 15;
+  public final double maxAcceleration = 15;
+  public final SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(?, ?);
+  final ProfiledPIDController extensionPID = new ProfiledPIDController(0.1, 0.0, 0.0, new Constraints(15, 100));
+  final ProfiledPIDController rotationPID = new ProfiledPIDController(0.1, 0.0, 0.0, new Constraints(15, 100));
 
-  
-	boolean[] _previous_currentBtns = new boolean[Constants.kNumButtonsPlusOne];
-	boolean[] _currentBtns = new boolean[Constants.kNumButtonsPlusOne];
-
-  boolean _firstCall = false; ? //idk what this is
-	boolean _state = false; ? //idk what this is
-	double targetAngleHigh = 0;
-  double targetAngleMedium = 0;
-  double targetAngleLow = 0;
-
-/** How much smoothing [0,8] to use during MotionMagic */
-int smoothing;
+	final double targetAngleHigh = 0;
+  final double targetAngleMedium = 0;
+  final double targetAngleLow = 0;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
 		leftArmMotor.setNeutralMode(NeutralMode.Brake);
 		rightArmMotor.setNeutralMode(NeutralMode.Brake);
+    rightArmMotor.follow(leftArmMotor);
 
-    leftArmMotor.set(ControlMode.Velocity, smoothing);
-    rightArmMotor.set(ControlMode.Velocity, smoothing);
-
-		leftArmMotor.configPeakOutputForward(+1.0);
-		leftArmMotor.configPeakOutputReverse(-1.0);
-		rightArmMotor.configPeakOutputForward(+1.0);
-		rightArmMotor.configPeakOutputReverse(-1.0);
-
+    ShuffleboardTab armTab = Shuffleboard.getTab("Arm");
+    ShuffleboardLayout rotationLayout = armTab.getLayout("Rotation");
+    rotationLayout.addDouble("RotationSpeed", () -> leftCANCoder.getVelocity()*360*10/2048);
+    rotationLayout.addDouble("RotationPosition", () -> leftCANCoder.getAbsolutePosition()*360/2048);
     
-		/* Set Motion Magic gains in slot0 - see documentation */
-		leftArmMotor.selectProfileSlot(Constants.kSlotIdx, Constants.kPIDLoopIdx);
-		leftArmMotor.config_kF(Constants.kSlotIdx, Constants.kGains.kF, Constants.kTimeoutMs);
-		leftArmMotor.config_kP(Constants.kSlotIdx, Constants.kGains.kP, Constants.kTimeoutMs);
-		leftArmMotor.config_kI(Constants.kSlotIdx, Constants.kGains.kI, Constants.kTimeoutMs);
-		leftArmMotor.config_kD(Constants.kSlotIdx, Constants.kGains.kD, Constants.kTimeoutMs);
+    ShuffleboardLayout extensionLayout = armTab.getLayout("Extension");
+    rotationLayout.addDouble("ExtensionSpeed", () -> leftCANCoder.getVelocity()*10/2048);
+    rotationLayout.addDouble("ExtensionPosition", () -> leftCANCoder.getAbsolutePosition()/2048);
+
   }
 
-  public void armUp() {
+  public void armPosition(double angle) {
+    double vSetpoint = rotationPID.calculate(2048*angle/360) / 360;
+    rightArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        rotationPID.calculate(2048*angle/360) / 360,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration))); //calculates max power output so as not to go above max velocity and max accel
+
+    leftArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        rotationPID.calculate(2048*angle/360) / 360,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration))); //calculates max power output so as not to go above max velocity and max accel
   }
 
-  public void armDown() {
+  public void armHighAngle() {
   }
 
-  public void armScoreHigh() {
+  public void armMiddleAngle() {
   }
 
-  public void armScoreMedium() {
-  }
-
-  public void armScoreLow() {
-  }
-
-  public void armStop() {
+  public void armLowAngle() {
   }
 
   @Override
   public void periodic() {
-    double leftYstick = -1.0 * oliviaController.getY(); /* left-side Y for Xbox360Gamepad */
-		double rghtYstick = -1.0 * oliviaController.getRawAxis(?); /* right-side Y for Xbox360Gamepad */
-		if (Math.abs(leftYstick) < 0.10) { 
-      leftYstick = 0; /* deadband 10% */
-    } 
-		if (Math.abs(rghtYstick) < 0.10) { 
-      rghtYstick = 0; /* deadband 10% */
-    } 
-    // This method will be called once per scheduler run
+    armPosition(askdaksdjasjdkasjd);
   }
 }

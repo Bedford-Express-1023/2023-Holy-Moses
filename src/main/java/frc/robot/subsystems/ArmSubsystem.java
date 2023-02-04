15c6eb7f9da6f174f69916a44e9920a93410f8a8
Stephen Oz <31784207+SteptimusHeap@users.noreply.github.com>
2023-02-04 10:02:11 -0500

    added extension

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -29,8 +29,11 @@ public class ArmSubsystem extends SubsystemBase {
   public final CANCoder rightCANCoder = new CANCoder(?);
 
   public final PigeonIMU pidgeonGyro = new PigeonIMU(0);
-  public final double maxVelocity = 15;
-  public final double maxAcceleration = 15;
+  public double armPosition = 0;
+  public final double maxRotationVelocity = 15;
+  public final double maxRotationAcceleration = 15;
+  public final double maxExtensionVelocity = 15;
+  public final double maxExtensionAcceleration = 15;
   public final SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(?, ?);
   final ProfiledPIDController extensionPID = new ProfiledPIDController(0.1, 0.0, 0.0, new Constraints(15, 100));
   final ProfiledPIDController rotationPID = new ProfiledPIDController(0.1, 0.0, 0.0, new Constraints(15, 100));
@@ -47,41 +50,77 @@ public class ArmSubsystem extends SubsystemBase {
 
     ShuffleboardTab armTab = Shuffleboard.getTab("Arm");
     ShuffleboardLayout rotationLayout = armTab.getLayout("Rotation");
-    rotationLayout.addDouble("RotationSpeed", () -> leftCANCoder.getVelocity()*360*10/2048);
-    rotationLayout.addDouble("RotationPosition", () -> leftCANCoder.getAbsolutePosition()*360/2048);
+    rotationLayout.addDouble("RotationSpeed", () -> ticksToDegrees(leftCANCoder.getVelocity() * 10));
+    rotationLayout.addDouble("RotationPosition", () -> ticksToDegrees(leftCANCoder.getAbsolutePosition()));
     
     ShuffleboardLayout extensionLayout = armTab.getLayout("Extension");
-    rotationLayout.addDouble("ExtensionSpeed", () -> leftCANCoder.getVelocity()*10/2048);
-    rotationLayout.addDouble("ExtensionPosition", () -> leftCANCoder.getAbsolutePosition()/2048);
+    rotationLayout.addDouble("ExtensionSpeed", () -> leftCANCoder.getVelocity()*10);
+    rotationLayout.addDouble("ExtensionPosition", () -> leftCANCoder.getAbsolutePosition());
 
+  }
+
+  public void armRotationPosition() {
+    double vSetpoint = rotationPID.calculate(2048*armPosition/360) / 360;
+    rightArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        vSetpoint,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxRotationVelocity, maxRotationVelocity), maxRotationAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxRotationVelocity, maxRotationVelocity), maxRotationAcceleration))); //calculates max power output so as not to go above max velocity and max accel
+    leftArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        vSetpoint,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxRotationVelocity, maxRotationVelocity), maxRotationAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxRotationVelocity, maxRotationVelocity), maxRotationAcceleration))); //calculates max power output so as not to go above max velocity and max accel
+  }
+
+  public void armExtensionPosition() {
+    double vSetpoint = extensionPID.calculate(2048*armPosition/360) / 360;
+    rightArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        vSetpoint,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxExtensionVelocity, maxRotationVelocity), maxRotationAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxRotationVelocity, maxRotationVelocity), maxRotationAcceleration))); //calculates max power output so as not to go above max velocity and max accel
+    leftArmMotor.set(ControlMode.PercentOutput, 
+      MathUtil.clamp(
+        vSetpoint,
+        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxExtensionVelocity, maxExtensionVelocity), maxExtensionAcceleration),
+        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxExtensionVelocity, maxExtensionVelocity), maxExtensionAcceleration))); //calculates max power output so as not to go above max velocity and max accel
   }
 
   public void armPosition(double angle) {
-    double vSetpoint = rotationPID.calculate(2048*angle/360) / 360;
-    rightArmMotor.set(ControlMode.PercentOutput, 
-      MathUtil.clamp(
-        rotationPID.calculate(2048*angle/360) / 360,
-        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration),
-        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration))); //calculates max power output so as not to go above max velocity and max accel
-
-    leftArmMotor.set(ControlMode.PercentOutput, 
-      MathUtil.clamp(
-        rotationPID.calculate(2048*angle/360) / 360,
-        -feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration),
-        feedForward.calculate(MathUtil.clamp(vSetpoint, -maxVelocity, maxVelocity), maxAcceleration))); //calculates max power output so as not to go above max velocity and max accel
+    armPosition = angle;
   }
 
-  public void armHighAngle() {
+  public void armHighScore() {
+    armPosition = targetAngleHigh;
   }
 
-  public void armMiddleAngle() {
+  public void armMiddleScore() {
+    armPosition = targetAngleMedium;
   }
 
-  public void armLowAngle() {
+  public void armLowScore() {
+    armPosition = targetAngleLow;
   }
+/**
+ * @param ticks the encoder value (in ticks, 2048/rotation)
+ * @return the angle, in degrees, off of absolute 0
+ */
+  public double ticksToDegrees(double ticks) {
+    return (360 / 2048)/*ungeared ticks to degrees */ / (2048 / 18); /*gear ratio */
+  }
+
+  /**
+ * @param ticks the encoder value (in ticks, 2048/rotation)
+ * @return the distance, in meters, travelled by the arm
+ */
+  public double ticksToMeters(double ticks) {
+    return (1/2048)/*ticks to rotations*/ * (8/60) * (18/35)/*gear ratios*/ * 0.0508*Math.PI/*roller rotations to meters of cord*/ * 2/*final pulley magic ratio*/;
+  }
+
 
   @Override
   public void periodic() {
-    armPosition(askdaksdjasjdkasjd);
+    armPosition(armPosition);
   }
 }

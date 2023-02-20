681f58e03540b0df635ded64dbd8a43e6f7c5d04
Shelby Howell <34521842+shelbymihow@users.noreply.github.com>
2023-02-20 18:43:52 -0500

    add autochooser and auto delay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,16 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import frc.robot.commands.*;
+import frc.robot.commands.Autos.DoNothing;
 import frc.robot.commands.Autos.GoToCone;
 import frc.robot.subsystems.*;
 
@@ -21,9 +26,14 @@ import frc.robot.subsystems.*;
 
     
 public class RobotContainer {
+    /* Sendable Choosers */
+    public final SendableChooser<Command> autoChooser = new SendableChooser<Command>();
+    public final SendableChooser<Command> autoDelay = new SendableChooser<Command>();
+
     /* Controllers */
     private final Joystick willController = new Joystick(0);
     private final XboxController oliviaController = new XboxController(1);
+    private final XboxController testController = new XboxController(4);
 
     private double slewDouble = 1000.0; //3.0
     private final SlewRateLimiter willSlew = new SlewRateLimiter(slewDouble);
@@ -37,9 +47,15 @@ public class RobotContainer {
     /* Driver Buttons */
     private final JoystickButton zeroGyro = new JoystickButton(willController, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(willController, XboxController.Button.kLeftBumper.value);
+    private final JoystickButton ArmDown = new JoystickButton(testController, XboxController.Button.kX.value);
+    //private final JoystickButton ArmUp = new JoystickButton(testController, XboxController.Button.kY.value);
+    private final JoystickButton WristTest = new JoystickButton(testController, XboxController.Button.kA.value);
+    private final JoystickButton WristTest2 = new JoystickButton(testController, XboxController.Button.kB.value);
+
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
+    //private final ArmSubsystem s_Arm = new ArmSubsystem();
 
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -54,6 +70,23 @@ public class RobotContainer {
             )
         );
 
+
+        autoDelay.setDefaultOption("none", new WaitCommand(0.0));
+        autoDelay.addOption("1.0", new WaitCommand(1.0));
+        autoDelay.addOption("2.0", new WaitCommand(2.0));
+        autoDelay.addOption("3.0", new WaitCommand(3.0));
+        autoDelay.addOption("4.0", new WaitCommand(4.0));
+        autoDelay.addOption("5.0", new WaitCommand(5.0));
+        autoDelay.addOption("6.0", new WaitCommand(6.0));
+        autoDelay.addOption("7.0", new WaitCommand(7.0));
+        autoDelay.addOption("8.0", new WaitCommand(8.0));
+        autoDelay.addOption("9.0", new WaitCommand(9.0));
+        autoDelay.addOption("10.0", new WaitCommand(10.0));
+        
+        SmartDashboard.putData(autoDelay);
+
+        autoChooser.setDefaultOption("Do Nothing", new DoNothing());
+
         // Configure the button bindings
         configureButtonBindings();
     }
@@ -67,6 +100,8 @@ public class RobotContainer {
     private void configureButtonBindings() {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
+        //ArmUp.onTrue(new InstantCommand(s_Arm::ArmHighScore));
+        //ArmDown.onTrue(new InstantCommand(s_Arm::ArmLowScore));
     }
 
     /**

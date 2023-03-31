7f700ca704013e9adaf36e491f18bb15ed88587c
Shelby Howell <34521842+shelbymihow@users.noreply.github.com>
2023-03-31 19:26:08 -0400

    doxygen arm subsystem and autonomous command

--- src/main/java/frc/robot/commands/Autos/BottomScore1CubeAnd1Cone.java
+++ src/main/java/frc/robot/commands/Autos/BottomScore1CubeAnd1Cone.java
@@ -1,29 +1,26 @@
 package frc.robot.commands.Autos;
 
-import java.time.Instant;
-
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.ArmToHome;
 import frc.robot.commands.IntakeCone;
-import frc.robot.commands.IntakeCube;
 import frc.robot.commands.OutakeCone;
 import frc.robot.commands.OutakeCube;
 import frc.robot.commands.ScoreHigh;
 import frc.robot.commands.ScoreLow;
-import frc.robot.commands.ScoreMid;
 import frc.robot.commands.ShoulderToHome;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.WristSubsystem;
-//walker was here
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class BottomScore1CubeAnd1Cone extends SequentialCommandGroup {
-  /** Creates a new Auto1. */
+  /** Creates a new BottomScore1CubeAnd1Cone. 
+   * @file test for doxygen
+   */
   public BottomScore1CubeAnd1Cone(Swerve s_Swerve, IntakeSubsystem s_Intake, ArmSubsystem s_Arm, WristSubsystem s_Wrist) {
     addCommands(
     (new ScoreHigh(s_Arm, s_Wrist).alongWith(new IntakeCone(s_Intake))).withTimeout(1),
@@ -31,12 +28,7 @@ public class BottomScore1CubeAnd1Cone extends SequentialCommandGroup {
     (new ArmToHome(s_Wrist, s_Arm))
         .withTimeout(.5),
     new PathPlannerCommand(s_Swerve, 2, "Back up after cone bottom", true)
-/*      
-.alongWith(new InstantCommand(() -> s_Arm.shoulderReversed *= -1))
-      .alongWith(new ScoreLow(s_Wrist, s_Arm))
-      .withTimeout(2),*/
       .deadlineWith(new ArmToHome(s_Wrist, s_Arm).andThen(new ShoulderToHome(s_Arm))),
-   
       new PathPlannerCommand(s_Swerve, 2, "Grab cube 1 Bottom")
       .alongWith(new InstantCommand(() -> s_Arm.shoulderReversed *= -1))
         .alongWith(new ScoreLow(s_Wrist, s_Arm))
